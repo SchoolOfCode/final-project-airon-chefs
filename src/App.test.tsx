e708e6063953c0f3fb7ad880985626741fb8ac11
Timothy Li <timothy93li@gmail.com>
2025-03-05 21:09:58 +0000

    test deploy fix, changed vite config for vercel to ignore tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders login page link", () => {
+test("renders login page test", () => {
   render(<App />);
   const linkElement = screen.getByText(/Login Page/i); // checks that an element with the text Login Page is rendered
   expect(linkElement).toBeInTheDocument();
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { join } from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  test: {
-    globals: true, // Enable global test functions like `describe`, `it`, `expect`
-    environment: "jsdom", // Use jsdom for simulating browser behavior in tests
-    include: ["**/*.test.tsx", "**/*.test.ts"], // Include test files with the .test.tsx or .test.ts extensions
-    setupFiles: [join(__dirname, "src/setupTests.ts")], // Optional: if you need any setup files
-  },
-});
+  ...(mode !== "production" && {
+    test: {
+      globals: true,
+      environment: "jsdom",
+      include: ["**/*.test.tsx", "**/*.test.ts"],
+      setupFiles: [join(__dirname, "src/setupTests.ts")],
+    },
+  }),
+}));
